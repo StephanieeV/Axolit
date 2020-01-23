6007a40bf9a6ad529e5ea31b453f6d6d595d3e35
Sylvie Nguyen <ng.sylvie@hotmail.com>
2020-01-23 13:29:05 +0100

    Modif annonce non fonctionnel

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class AnnonceController extends AbstractController
@@ -38,7 +39,7 @@ class AnnonceController extends AbstractController
      * //@todo suggérer l'inscription sute  à denyAccessUnlessgranted 
      * @Route("/vendre", name="vendre")
      */
-    public function vendre(Request $request )
+    public function vendre(Request $request)
     {   
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $annonce = new Annonce();
@@ -63,5 +64,39 @@ class AnnonceController extends AbstractController
             return $this->redirectToRoute('accueil');
         }
         return $this->render('./default/annonce/vendre.html.twig', array('form_annonce' => $form->createView()));
+    }
+
+    /**
+     * @Route("/{id}/edit", name="annonce_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Annonce $annonce): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('mes_annonces');
+        }
+
+        return $this->render('./default/annonce/mes_annonces.html.twig', [
+            'annonce' => $annonce,
+            'form_annonce' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="annonce_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Annonce $annonce): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$annonce->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($annonce);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('mes_annonces');
     }
 }
